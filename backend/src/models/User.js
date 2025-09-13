d583270fca9ba5809b9dcf81d3e397e8933f6568
Naveen <i.guptanaveen@gmail.com>
2025-09-13 14:33:15 +0530

    fixing schema

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-  userId: { type: Number, required: true, unique: true },
+  userId: { type: Number, unique: true },
   name: { type: String, required: true },
-  phone: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  phone: { type: String},
+  email: { type: String, unique: true },
   status: { type: String, enum: ['active', 'inactive'], default: 'active' },
   isAdmin: { type: Boolean, default: false }
 }, { timestamps: true });
